f2c9f9f5180f23baa1b7154180e2aac7f50025e6
Mattia1689 <info@antoniottiodilio.com>
2026-07-15 23:30:40 +0200

    Summary
    Versione 0.1 - Mappa iniziale

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
+const map = L.map('map').setView([45.628, 8.299], 14);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Carica i vigneti
+fetch('data/vigneti.geojson')
+    .then(response => response.json())
+    .then(data => {
+
+        L.geoJSON(data, {
+
+            onEachFeature: function(feature, layer) {
+
+                layer.bindPopup(`
+                    <h3>${feature.properties.nome}</h3>
+
+                    <b>Località:</b> ${feature.properties.localita}<br>
+
+                    <b>Comune:</b> ${feature.properties.comune}<br>
+
+                    <b>DOC:</b> ${feature.properties.doc}
+                `);
+
+            }
+
+        }).addTo(map);
+
+    });
